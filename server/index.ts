c19c05dc4a94b6a6f0e0ef0da8445852836fcc8a
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-05-12 14:42:43 +0530

    basic structure/LoginPage

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+import http from "http";
+import cors from "cors";
+
+const app=express();
+const server=http.createServer(app);
+const {PORT} =process.env || 4000
+
+app.use(cors());
+
+server.listen(PORT,()=>{
+    console.log(`Server started at port ${PORT}`)
+})
